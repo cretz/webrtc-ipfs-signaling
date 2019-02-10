7762961df970becfba047cdb789c49f93762a4d1
Chad Retz <chad.retz@gmail.com>
2019-02-09 22:09:41 -0600

    More work w/ DHT

---
+++ README.md
@@ -0,0 +1,4 @@
+Early sandbox repo playing around w/ getting signaling to work w/ WebRTC via IPFS.
+
+Result: IPFS is close but not quite ready for DHT. It doesn't have any bootstrappable peers that I can see and the built
+JS from current master is like 2.5MB minimized. Will be waiting...
--- browser.js
+++ browser.js
@@ -1,4 +1,6 @@
 
+const debug = console.log
+
 function createWindowHashIfNotPresent() {
   if (window.location.hash) return
   const base58Chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
@@ -8,6 +10,35 @@ function createWindowHashIfNotPresent() {
   window.history.replaceState(null, null, '#' + String.fromCharCode.apply(null, array))
 }
 
+function newIPFS(cb) {
+  const ipfs = new Ipfs({
+    // repo: String(Math.random() + Date.now()),
+    // libp2p: {
+    //   config: {
+    //     dht: {
+    //       enabled: true
+    //     }
+    //   }
+    // },
+    // relay: {
+    //   enabled: true,
+    //   hop: {enabled: true}
+    // }
+  })
+  // Wait for peers
+  ipfs.on('ready', () => {
+    const tryListPeers = () => {
+      ipfs.swarm.peers((err, peers) => {
+        if (err) throw err
+        debug('Peers', peers)
+        if (!peers || peers.length == 0) setTimeout(() => tryListPeers(), 1000)
+        else cb(ipfs)
+      })
+    }
+    tryListPeers()
+  })
+}
+
 function ipfsDirBase() {
   return window.location.hash.substring(1)
 }
@@ -15,19 +46,49 @@ function ipfsDirBase() {
 function writeIPFSFile(ipfs, file, content, cb) {
   const path = '/' + ipfsDirBase() + '/' + file
   const buf = ipfs.types.Buffer.from(content)
+  debug('Writing to ' + path)
+  writeIPFSFileMFS(ipfs, path, buf, cb)
+  // writeIPFSFileDHT(ipfs, path, buf, cb)
+}
+
+function writeIPFSFileMFS(ipfs, path, buf, cb) {
   ipfs.files.write(path, buf, { create: true, truncate: true, parents: true}).then(() => {
     console.log('Wrote to ' + path)
     cb()
   }).catch(console.error)
 }
 
+function writeIPFSFileDHT(ipfs, path, buf, cb) {
+  ipfs.dht.put(ipfs.types.Buffer.from(path), buf, (err) => {
+    if (err) throw err
+    console.log('Wrote to ' + path)
+    cb()
+  })
+}
+
 function waitForIPFSFile(ipfs, file, cb) {
   const path = '/' + ipfsDirBase() + '/' + file
+  debug('Attempting to read from ' + path)
+  waitForIPFSFileMFS(ipfs, path, cb)
+  // waitForIPFSFileDHT(ipfs, path, cb)
+}
+
+function waitForIPFSFileMFS(ipfs, path, cb) {
   ipfs.files.read(path, (err, buf) => {
     if (err) {
-      if (!err.message || !err.message.endsWith(file + ' does not exist')) throw err
-      setTimeout(() => waitForIPFSFile(ipfs, file, cb), 2000)
+      debug('Failed reading', err.message)
+      if (!err.message || !err.message.endsWith(path + ' does not exist')) throw err
+      setTimeout(() => waitForIPFSFileMFS(ipfs, path, cb), 2000)
     } else cb(buf.toString('utf8'))
+  })
+}
+
+function waitForIPFSFileDHT(ipfs, path, cb) {
+  ipfs.dht.get(ipfs.types.Buffer.from(path), (err, value) => {
+    if (err) {
+      debug('Failed reading', err.message)
+      setTimeout(() => waitForIPFSFileDHT(ipfs, path, cb), 2000)
+    } else cb(value.toString('utf8'))
   })
 }
 
